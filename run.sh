ff1ecb5ef05087d15f7cfff9e3fc928c68d6a7c9
nandankita <nandankita@gmail.com>
2025-05-28 18:07:10 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,34 +1,69 @@
 #!/bin/bash
-#SBATCH -p himem
-#SBATCH --mem=60G
-#SBATCH -t 12:00:00
-#SBATCH -J Remap2022
+#BATCH -p himem
+#SBATCH --mem=30G
+#SBATCH -t 48:00:00
+#SBATCH -J Remap2020_TFs
 #SBATCH -o %x-%j.out
 #SBATCH -e %x-%j.err
 #SBATCH -c 1
 
-echo "running LOLA"
-
+#-------------------------------------------------------------------------------------------------------------------------------------
+-
+# Script adapted from Aditi's script 13_runLOLA_Run_20190402 designed to assess enrichment of TF (ChIP-seq - Remap) on TEs of interest
+# Background of analysis: all TEs overlapping the dataset of interest (e.g. H3K27ac PCa)
+# Run this script on h4h - no LOLA package on mordor!!!
+#-------------------------------------------------------------------------------------------------------------------------------------
+-
 
 module load bedtools/2.23.0
-module load R/3.4.1
+module load R/3.6.1
 
-loladb="hg38_remapdb_2022"
+
+#----------------------------------------------------------------
+## LOLA Databases
+#----------------------------------------------------------------
+
+loladb="hg38_remap2022/"
+
+#----------------------------------------------------------------
+## Run LOLA on each repeat subfam
+#----------------------------------------------------------------
+
+# input the path of each TE subfamily of interest - on h4h
+queryfiles="/cluster/projects/lupiengroup/People/ggrillo/data/repeat.subfam/hg38_only_TEs/1138_MIR1_Amn.bed.sorted
+/cluster/projects/lupiengroup/People/ggrillo/data/repeat.subfam/hg38_only_TEs/1141_MIR.bed.sorted
+/cluster/projects/lupiengroup/People/ggrillo/data/repeat.subfam/hg38_only_TEs/1140_MIRb.bed.sorted
+/cluster/projects/lupiengroup/People/ggrillo/data/repeat.subfam/hg38_only_TEs/1142_MIRc.bed.sorted
+/cluster/projects/lupiengroup/People/ggrillo/data/repeat.subfam/hg38_only_TEs/1161_MLT1F1.bed.sorted
+/cluster/projects/lupiengroup/People/ggrillo/data/repeat.subfam/hg38_only_TEs/1163_MLT1F2.bed.sorted
+/cluster/projects/lupiengroup/People/ggrillo/data/repeat.subfam/hg38_only_TEs/1164_MLT1F2-int.bed.sorted
+/cluster/projects/lupiengroup/People/ggrillo/data/repeat.subfam/hg38_only_TEs/1165_MLT1F.bed.sorted
+/cluster/projects/lupiengroup/People/ggrillo/data/repeat.subfam/hg38_only_TEs/1169_MLT1G3.bed.sorted
+/cluster/projects/lupiengroup/People/ggrillo/data/repeat.subfam/hg38_only_TEs/1202_MLT2C2.bed.sorted
+/cluster/projects/lupiengroup/People/ggrillo/data/repeat.subfam/hg38_only_TEs/1373_Tigger12c.bed.sorted
+/cluster/projects/lupiengroup/People/ggrillo/data/repeat.subfam/hg38_only_TEs/1375_Tigger14a.bed.sorted"
+
 
 for db in $loladb ;
-do  
-    dbname="remap2022"
-    queryname=sample1
-    opname=$queryname"_bg_catpeaks_"$dbname
-    echo "----------------------------------------------------------------"
-    echo "File being analysed is " $opname
-    echo "----------------------------------------------------------------"
-    mkdir -p results/$opname
+do
+    dbname=$(echo $db | awk -F"/" '{print $(NF-2)}')
+    echo $dbname
+    for query in $queryfiles ;
+    do
+        queryname=$(echo $query | awk -F"/" '{print $NF}' | sed -e 's/.bed.sorted//g')
+        opname=$queryname"_"$dbname
+        echo "----------------------------------------------------------------"
+        echo "File being analysed is " $opname
+        echo "----------------------------------------------------------------"
+        mkdir -p $opname
 
+       intersectBed -a $query -b consensus.bed -u > $opname/querytmp.bed
 
-    Rscript lola.R \
-            sample1.bed \
-            consensusbackground.bed \
-            $db \
-            results/$opname
+       Rscript lola.R \
+       $opname/querytmp.bed \
+       consensus.bed \
+       $db \
+       $opname
+    done
 done
+
